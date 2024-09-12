a5b607d4113180c0a015c59d04581c790e30f38d
Artem-Brui <artembryj@gmail.com>
2024-09-12 17:22:20 +0200

    update age field

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,8 +1,6 @@
 export const Person = ({
   person: { name, age, sex, isMarried, partnerName },
 }) => {
-  const personAge = age ? <p className="Person__age">I am {age}</p> : null;
-
   const partnerType = sex === 'm' ? 'wife' : 'husband';
 
   const personPartner = isMarried
@@ -12,7 +10,7 @@ export const Person = ({
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {personAge}
+      {age} && <p className="Person__age">I am {age}</p>
       <p className="Person__partner">{personPartner}</p>
     </section>
   );
